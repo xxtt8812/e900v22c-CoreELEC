3ad0c8c6d73739a2f0c2a7bcc5a6d7a2da3a3010
xxtt8812 <105150612+xxtt8812@users.noreply.github.com>
2023-01-05 10:05:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-version="19.5-Matrix_rc3"
-source_img_name="CoreELEC-Amlogic-ng.arm-${version}-Generic"
-source_img_file="${source_img_name}.img.gz"
-source_img_url="https://github.com/CoreELEC/CoreELEC/releases/download/${version}/${source_img_name}.img.gz"
+version="19.4.1"
+source_img_name="CoreELEC-Amlogic-ng.arm-${version}.tar"
+source_img_file="${source_img_name}.tar"
+source_img_url="https://github.com/RuralHunter/CoreELEC/releases/download/${version}/${source_img_name}.tar"
 target_img_prefix="CoreELEC-Amlogic-ng.arm-${version}"
 target_img_name="${target_img_prefix}-E900V22C-$(date +%Y.%m.%d)"
 mount_point="target"
